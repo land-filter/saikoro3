b1a93fff7fae89fa9d9cb2617816e8159eb38555
land-filter <106000476+land-filter@users.noreply.github.com>
2024-02-11 18:06:39 +0900

    Delete saikorougoki.js

--- saikorougoki.js
+++
@@ -1,7 +0,0 @@
-function rollDice() {
-    // サイコロを振る結果をランダムに生成（1から6の整数）
-    var result = Math.floor(Math.random() * 6) + 1;
-
-    // 結果を表示
-    document.getElementById("diceResult").innerText = result;
-}
